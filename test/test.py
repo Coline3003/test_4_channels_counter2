9e39563dc28405045e85ae51efc991bd7eb830a3
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-07-09 11:47:38 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,56 +27,31 @@ async def test_project(dut):
 
     # Set the input values you want to test
     # set the RTC_clk to 3600 Hz to have a sending each second instead of each hour
-    RTC_clk = Clock(dut.ui_in[0], 277777778, units="ps")
+    RTC_clk = Clock(dut.ui_in[6], 277777778, units="ps")
     cocotb.start_soon(RTC_clk.start())
+
+    # set the main clk to 1 MHz 
+    clk = Clock(dut.ui_in[1], 1, units="us")
+    cocotb.start_soon(clk.start())
+
+    # set the reset signal
+    reset = Clock(dut.ui_in[0], 900, units="ms")
+    cocotb.start_soon(clk.start())
 
     
     #set values for channels 
     #channel 1 => 1 pulse 
-    ch1 = Clock(dut.ui_in[1], 1, units="sec")
+    ch1 = Clock(dut.ui_in[2], 1, units="sec")
     cocotb.start_soon(ch1.start())
     #channel 2 => 2 pulses 
-    ch2 = Clock(dut.ui_in[2], 500, units="ms")
+    ch2 = Clock(dut.ui_in[3], 500, units="ms")
     cocotb.start_soon(ch2.start())
     #channel 3 => 4 pulses 
-    ch3 = Clock(dut.ui_in[3], 250, units="ms")
+    ch3 = Clock(dut.ui_in[4], 250, units="ms")
     cocotb.start_soon(ch3.start())
     #channel 4 => 8 pulses 
-    ch4 = Clock(dut.ui_in[4], 125, units="ms")
+    ch4 = Clock(dut.ui_in[5], 125, units="ms")
     cocotb.start_soon(ch4.start())
-    #channel 5 => 16 pulses 
-    ch5 = Clock(dut.ui_in[5], 62500, units="us")
-    cocotb.start_soon(ch5.start())
-    #channel 6 => 32 pulses 
-    ch6 = Clock(dut.ui_in[6],31250 , units="us")
-    cocotb.start_soon(ch6.start())
-    #channel 7 => 64 pulses 
-    ch7 = Clock(dut.ui_in[7],15625 , units="us")
-    cocotb.start_soon(ch7.start())
-    #channel 8 => 128 pulses 
-    ch8 = Clock(dut.uio_in[0],7812500 , units="ns")
-    cocotb.start_soon(ch8.start())
-    #channel 9 => 256 pulses 
-    ch9 = Clock(dut.uio_in[1], 3906250, units="ns")
-    cocotb.start_soon(ch9.start())
-     #channel 10 => 512 pulses 
-    ch10 = Clock(dut.uio_in[2], 1953125, units="ns")
-    cocotb.start_soon(ch10.start())
-      #channel 11 => 1024 pulses 
-    ch11 = Clock(dut.uio_in[3], 976562500, units="ps")
-    cocotb.start_soon(ch11.start())
-     #channel 12 => 2048 pulses 
-    ch12 = Clock(dut.uio_in[4],488281250 , units="ps")
-    cocotb.start_soon(ch12.start())
-     #channel 13 => 2730 pulses (1010 1010 1010)b
-    ch13 = Clock(dut.uio_in[5], 366300, units="ns")
-    cocotb.start_soon(ch13.start())
-     #channel 14 => 240 pulses (0000 1111 0000)b
-    ch14 = Clock(dut.uio_in[6], 4166666, units="ns")
-    cocotb.start_soon(ch14.start())
-     #channel 15 => 4095 pulses (1111 1111 1111)b
-    ch15 = Clock(dut.uio_in[7], 244200, units="ns")
-    cocotb.start_soon(ch15.start())
 
 
     # Wait 
@@ -85,9 +60,9 @@ async def test_project(dut):
 
     # test ovf channel
 
-     #channel 15 => 4096 pulses 
-    ch15 = Clock(dut.uio_in[7], 244, units="us")
-    cocotb.start_soon(ch15.start())
+     #channel 1 => 4096 pulses 
+    ch1 = Clock(dut.uio_in[2], 244, units="us")
+    cocotb.start_soon(ch1.start())
 
     # Wait to see the overflow
     await ClockCycles(dut.clk,1000000)
